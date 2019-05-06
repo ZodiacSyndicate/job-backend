bd1981c3c430da6a57dc567482817b94c2548d9e
zodiacsyndicate <zodiacsyndicate@163.com>
2019-05-06 17:58:25 +0800

    add list login info api

--- controllers/user.go
+++ controllers/user.go
@@ -11,6 +11,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+const cookieKey = "USER_ID"
+
 type Base struct {
 	ID        uuid.UUID  `gorm:"primary_key"`
 	CreatedAt time.Time  `json:"created_at"`
@@ -18,9 +20,8 @@ type Base struct {
 	DeletedAt *time.Time `sql:"index" json:"deleted_at"`
 }
 
-func (b *User) BeforeCreate(scope *gorm.Scope) error {
-	err := scope.SetColumn("ID", uuid.New())
-	if err != nil {
+func (u *User) BeforeCreate(scope *gorm.Scope) error {
+	if err := scope.SetColumn("ID", uuid.New()); err != nil {
 		return err
 	}
 	return nil
@@ -29,14 +30,25 @@ func (b *User) BeforeCreate(scope *gorm.Scope) error {
 // User table
 type User struct {
 	Base
-	Username string `gorm:"unique;NOT NULL" json:"username"`
-	Password string `gorm:"NOT NULL"`
-	Type     string `gorm:"NOT NULL" json:"type"`
-	Avatar   string `json:"avatar"`
-	Desc     string `json:"desc"`
-	Title    string `json:"title"`
-	Company  string `json:"company"`
-	Money    string `json:"money"`
+	Username    string `gorm:"unique;NOT NULL" json:"username"`
+	Password    string `gorm:"NOT NULL"`
+	Type        string `gorm:"NOT NULL" json:"type"`
+	Avatar      string `json:"avatar"`
+	Description string `json:"desc"`
+	Title       string `json:"title"`
+	Company     string `json:"company"`
+	Money       string `json:"money"`
+}
+
+type userResponse struct {
+	ID          uuid.UUID `json:"id"`
+	Username    string    `json:"username"`
+	Type        string    `json:"type"`
+	Avatar      string    `json:"avatar"`
+	Description string    `json:"desc"`
+	Title       string    `json:"title"`
+	Company     string    `json:"company"`
+	Money       string    `json:"money"`
 }
 
 type registerRequest struct {
@@ -65,21 +77,97 @@ func Register(ctx echo.Context) error {
 	user := User{Username: req.Username, Password: md5Pwd(req.Password), Type: req.Type}
 	err := db.Model(&User{}).First(&user, &User{Username: req.Username}).Error
 	if err == gorm.ErrRecordNotFound {
-		db.Create(&user)
-		return ctx.JSON(http.StatusOK, &response{
-			Code: 0,
-			Msg:  "",
-			Data: registerResponse{
-				Username: user.Username,
-				Type:     user.Type,
-				ID:       user.Base.ID,
-			},
-		})
+		e := db.Create(&user).Error
+		if e == nil {
+			ctx.SetCookie(&http.Cookie{Name: cookieKey, Value: user.Base.ID.String()})
+			return ctx.JSON(http.StatusOK, &response{
+				Code: 0,
+				Msg:  "",
+				Data: registerResponse{
+					Username: user.Username,
+					Type:     user.Type,
+					ID:       user.Base.ID,
+				},
+			})
+		}
+		return e
+	}
+	if err != nil {
+		return err
 	}
 	res := &response{
 		Code: 1,
 		Msg:  "User name has been taken",
-		Data: nil,
 	}
 	return ctx.JSON(http.StatusBadRequest, res)
+}
+
+type loginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Login handle user login
+func Login(ctx echo.Context) error {
+	req := new(loginRequest)
+	var (
+		err  error
+		user User
+		res  userResponse
+	)
+	if err = ctx.Bind(req); err != nil {
+		return err
+	}
+	err = db.Model(&User{}).First(&user, &User{Username: req.Username}).Scan(&res).Error
+	if err == gorm.ErrRecordNotFound {
+		return ctx.JSON(http.StatusBadRequest, &response{
+			Code: 1,
+			Msg:  "Username dose not exist",
+		})
+	}
+	if err != nil {
+		return err
+	}
+	if md5Pwd(req.Password) != user.Password {
+		return ctx.JSON(http.StatusBadRequest, &response{
+			Code: 1,
+			Msg:  "Wrong password",
+		})
+	}
+	ctx.SetCookie(&http.Cookie{Name: cookieKey, Value: user.Base.ID.String()})
+	return ctx.JSON(http.StatusOK, &response{
+		Code: 0,
+		Msg:  "",
+		Data: res,
+	})
+}
+
+// Info handle get info
+func Info(ctx echo.Context) error {
+	var res userResponse
+	cookie, err := ctx.Cookie(cookieKey)
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, &response{Code: 1})
+	}
+	err = db.Model(&User{}).Where("id = ?", cookie.Value).Scan(&res).Error
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, &response{Code: 1})
+	}
+	return ctx.JSON(http.StatusOK, &response{
+		Code: 0,
+		Data: res,
+	})
+}
+
+// List get user list
+func List(ctx echo.Context) error {
+	var res []*userResponse
+	err := db.Model(&User{}).Where("type = ?", ctx.QueryParams().Get("type")).Scan(&res).Error
+	if err != nil {
+		return ctx.JSON(http.StatusBadRequest, &response{Code: 1})
+	}
+	return ctx.JSON(http.StatusOK, &response{
+		Code: 0,
+		Data: res,
+	})
 }
--- server.go
+++ server.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"job-backend/controllers"
 
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 )
 
@@ -10,5 +12,12 @@ func main() {
 	e := echo.New()
 	user := e.Group("/user")
 	user.POST("/register", controllers.Register)
+	user.POST("/login", controllers.Login)
+	user.GET("/info", controllers.Info)
+	user.GET("/list", controllers.List)
+	user.GET("/", func(c echo.Context) error {
+		fmt.Println(uuid.New().String())
+		return nil
+	})
 	e.Logger.Fatal(e.Start(":3500"))
 }
